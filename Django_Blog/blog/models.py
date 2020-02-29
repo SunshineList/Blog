37892693c9274e3eecad64f05461c61e7ad88379
wangzw <wzw571603974@qq.com>
2020-02-29 20:22:53 +0800

    添加出版日期

--- Django_Blog/blog/models.py
+++ Django_Blog/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32)
     content = models.TextField(null=True)
-    pub_time = models.DateTimeField(null=True, auto_created=True)
+    pub_time = models.DateTimeField(null=True, auto_created=True, auto_now_add=True, )
 
     def __str__(self):
         return self.title
