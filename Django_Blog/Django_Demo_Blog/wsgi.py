dd5fe8a25c4e5d7a72d319d7a50b4360843ec778
wangzw <wzw571603974@qq.com>
2020-03-01 13:54:40 +0800

    添加出版日期

--- Django_Blog/Django_Demo_Blog/wsgi.py
+++ Django_Blog/Django_Demo_Blog/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_Demo_Blog.settings')
-
+sys.path.append('~/.pyenv/versions/3.7.5/lib/python3.7/site-packages')
 application = get_wsgi_application()
